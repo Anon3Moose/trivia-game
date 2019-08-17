c82689ecd6d281d4cfd18c9631804ce5653d99f4
Vestige <jaracomail@gmail.com>
2019-08-17 12:57:26 -0700

    game commit

---
+++ assets/javascript/questions.js
@@ -0,0 +1,71 @@
+var quizQuestion = (
+  {
+    question: "What is my full name?",
+    choices: ["Jared Acosta", "Jared", "Acosta", "Dr. Strange"],
+    correctAnswer: "Jared Acosta"
+  },
+
+  {
+    question: "What is a Geiger Counter used to detect??",
+    choices: ["Water", "Alcohol", "Radiation", "Ph"],
+    correctAnswer: "Radiation"
+  },
+
+  {
+    question: "Spinach is high in which mineral?",
+    choices: ["Iron", "Vitamin C", "Lead", "Aluminium"],
+    correctAnswer: "Iron"
+  },
+
+  {
+    question: "Which type of dog has breeds called Scottish, Welsh, and Irish?",
+    choices: ["Dane", "Sheperd", "Terrier", "Retriever"],
+    correctAnswer: "Terrier"
+  },
+  {
+    question: "Babe Ruth is associated with which sport?",
+    choices: ["Football", "Baseball", "Soccer", "Swimming"],
+    correctAnswer: "Baseball"
+  },
+  {
+    question: "In the film Babe, what type of animal was Babe?",
+    choices: ["Cow", "Lion", "Pig", "Horse"],
+    correctAnswer: "Pig"
+  },
+  {
+    question: "What was Mohammed Ali's birth name?",
+    choices: ["Alen", "Bruce Lee", "John Wick", "Cassius Clay"],
+    correctAnswer: "Cassius Clay"
+  },
+  {
+    question: "Which Roman emperor supposedly fiddled while Rome burned?",
+    choices: ["Arthur", "Siegfried", "Astolfo", "Nero"],
+    correctAnswer: "Nero"
+  },
+  {
+    question: "Which crime-fighting cartoon dog has the initals 'S.D.' on his collar?",
+    choices: ["Krypto", "Courage", "Scooby-Doo", "Underdog"],
+    correctAnswer: "Scooby-Doo"
+  },
+
+
+  {
+    question: "How many people are there in the world?",
+    choices: ["7.53B", "2.13T", "10", "984M"],
+    correctAnswer: "7.53B"
+  }
+);
+
+var funImages = (
+  './assets/images/happy fun.gif',
+  './assets/images/hellYeah.gif',
+  './assets/images/winning.gif',
+  './assets/images/yeah.gif',
+  './assets/images/you did it.gif'
+);
+
+var sadImages = (
+  './assets/images/danm.gif',
+  './assets/images/oops.gif',
+  './assets/images/pretty bad.gif'
+);
